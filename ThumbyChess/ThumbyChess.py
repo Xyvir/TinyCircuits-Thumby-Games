5fa19060f496f7eb3fc1ea8cabcdcddac2f6f76c
Xyvir <tymills620@gmail.com>
2023-01-09 21:47:53 -0600

    Update ThumbyChess.py

--- ThumbyChess/ThumbyChess.py
+++ ThumbyChess/ThumbyChess.py
@@ -439,8 +439,8 @@ class Searcher:
 ###############################################################################
 
 # Python 2 compatability
-if sys.version_info[0] == 2:
-    input = raw_input
+#if sys.version_info[0] == 2:
+#    input = raw_input
 
 
 def parse(c):
@@ -454,9 +454,7 @@ def render(i):
 
 
 def print_pos(pos):
-    print("")
-    uni_pieces = {'R':'♜', 'N':'♞', 'B':'♝', 'Q':'♛', 'K':'♚', 'P':'♟',
-                  'r':'♖', 'n':'♘', 'b':'♗', 'q':'♕', 'k':'♔', 'p':'♙', '.':'·'}
+    uni_pieces = {'R':'R', 'N':'N', 'B':'B', 'Q':'Q', 'K':'K', 'P':'P','r':'r', 'n':'n', 'b':'b', 'q':'q', 'k':'k', 'p':'p', '.':'.'}
     for i, row in enumerate(pos.board.split()):
         print(' ', 8-i, ' '.join(uni_pieces.get(p, p) for p in row))
     print('    a b c d e f g h \n\n')
