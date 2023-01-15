456e90f967007db80a6935bedfb901ef4b9f678c
Xyvir <tymills620@gmail.com>
2023-01-15 09:59:51 -0600

    Update DeepMu.py

--- DeepMu/DeepMu.py
+++ DeepMu/DeepMu.py
@@ -199,7 +199,7 @@ MATE_LOWER = piece['K'] - 10*piece['Q']
 MATE_UPPER = piece['K'] + 10*piece['Q']
 
 # The table size is the maximum number of elements in the transposition table.
-TABLE_SIZE = 150
+TABLE_SIZE = 120
 
 # Constants for tuning search
 QS_LIMIT = 219
